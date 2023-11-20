61b38e779e8d83d18fccdd5a1336f3e51fa07524
Iván <ivarcass@gmail.com>
2023-11-20 10:09:58 +0100

    Constructor PrimeraClase

--- Comedor.java
+++ Comedor.java
@@ -2,8 +2,9 @@ public class Comedor{
     private int horaApertura;
     private int horaCierre;
     private Jefe encargado;
-    public Comedor(){
-    
-        
+    public Comedor(int horaDeApertura, int horaDeCierre, int anosExperiencia, String nombreJefe){
+        horaApertura = horaDeApertura;
+        horaCierre = horaDeCierre;
+        encargado = new Jefe(nombreJefe, anosExperiencia);
     }
 }
